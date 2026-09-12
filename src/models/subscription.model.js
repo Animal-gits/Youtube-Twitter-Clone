ff7e3429cdbf7932933280025d85cbc71f9b408d
Animal-gits <muhammadhadi8920@gmail.com>
2026-09-12 19:06:47 +0500

    Created the subscription schema and created the change user password functionality

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import mongosose from "mongoose"
+
+const subscriptionSchema = new mongoose.Schema({
+    subscriber : {
+        type : mongoose.Schema.Types.ObjectId, // the one who is subscribing
+        ref : "User"
+    },
+    channel : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    }
+})
+
+const Subscription = mongoose.model("Subscription" , subscriptionSchema)
+
+export {Subscription}
