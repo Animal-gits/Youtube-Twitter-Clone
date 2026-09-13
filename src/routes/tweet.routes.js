2fef5d14bfe3a1242656bfedd5e87ae6038e1d15
Animal-gits <muhammadhadi8920@gmail.com>
2026-09-13 16:40:01 +0500

    Made the api to delete the tweet

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { createTweet, getMyTweets , getUserTweets, updateTweet } from "../controllers/tweet.controller";
+import { createTweet, deleteTweet, getMyTweets , getUserTweets, updateTweet } from "../controllers/tweet.controller";
 import { protect } from "../middlewares/auth.middleware";
 
 const router = Router()
@@ -8,5 +8,6 @@ router.post("/add_tweet" , protect ,createTweet)
 router.get("/tweet/:userId" , protect , getUserTweets )
 router.get("/my_tweets" , protect , getMyTweets )
 router.patch("/update_tweet" , protect , updateTweet )
+router.delete("/delete_tweet"  , protect , deleteTweet)
 
 export {router}
