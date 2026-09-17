74894181a592005a957ca50cd4a5f15148f3e222
Animal-gits <muhammadhadi8920@gmail.com>
2026-09-17 14:09:08 +0500

    made an api to add a video to a playlist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,73 +1,110 @@
 import mongoose from "mongoose";
 import { ApiError } from "../helpers/ApiError.js";
 import { ApiResponse } from "../helpers/ApiResponse.js";
-import {asyncHandler} from "../helpers/asyncHandler.js"
+import { asyncHandler } from "../helpers/asyncHandler.js"
 import { Playlist } from "../models/playlist.model.js";
 
-const createPlaylist = asyncHandler(async (req , res) => {
-    const {name , description} = req.body
-    if(!name || name.trim() === ""){
-        throw new ApiError(400 , "Enter Titl please")
+const createPlaylist = asyncHandler(async (req, res) => {
+    const { name, description } = req.body
+    if (!name || name.trim() === "") {
+        throw new ApiError(400, "Enter Titl please")
     }
 
-    if(!description || description.trim() === ""){
-        throw new ApiError(400 , "Enter description please")
+    if (!description || description.trim() === "") {
+        throw new ApiError(400, "Enter description please")
     }
 
     const playlist = await Playlist.create({
-        name, 
-        descriprion,
-        owner : req.user._id,
-        videos : []
+        name,
+        description,
+        owner: req.user._id,
+        videos: []
     })
 
-    if(!playlist){
-        throw new ApiError(400 , "Playlist not created")
+    if (!playlist) {
+        throw new ApiError(400, "Playlist not created")
     }
 
     res
         .status(200)
         .json(
-            new ApiResponse(200, playlist , "Playlist created successfully")
+            new ApiResponse(200, playlist, "Playlist created successfully")
         )
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
+    const { userId } = req.params
 
-    if(!mongoose.isValidObjectId(userId)){
-        throw new ApiError(400 , "Invalid Object Id")
+    if (!mongoose.isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid Object Id")
     }
 
     const playlist = await Playlist.find({
-        owner : req.user._id
+        owner: req.user._id
     })
 
-    if(!playlist){
-        throw new ApiError(400 , "Failed to find playlist for user")
+    if (!playlist) {
+        throw new ApiError(400, "Failed to find playlist for user")
     }
 
     res
         .status(200)
         .json(
-            new ApiResponse(200 , playlist  , "Playlists fetched successfully")
+            new ApiResponse(200, playlist, "Playlists fetched successfully")
         )
 })
 
-const getPlaylistById = asyncHandler(async (req , res) => {
-    const {playlistId} = req.params
+const getPlaylistById = asyncHandler(async (req, res) => {
+    const { playlistId } = req.params
 
-    if(!mongoose.isValidObjectId(playlistId)){
-        throw new ApiError(400 , "Invalid Playlist Id")
+    if (!mongoose.isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid Playlist Id")
     }
 
     const playlist = await Playlist.findById({
         playlistId
     })
+
+    if (!playlist) {
+        throw new ApiError(400, "Failed to get playlist")
+    }
+
+    res
+        .status(200)
+        .json(200, playlist, "Playlist fetched successfully")
+})
+
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+    const { playlistId, videoId } = req.params
+
+    if (!mongoose.isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid Playlist Id")
+    }
+
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid Video Id")
+    }
+
+    const playlist = await Playlist.findByIdAndUpdate({
+        playlistId
+    }, {
+        $addToSet: {
+            videos: videoId
+        }
+    }, { new: true })
+
+    if(!playlist){
+        throw new ApiError(400 , "Failed to add video to playlist")
+    }
+
+    res
+        .status(200)
+        .json(200 , playlist , "Successfully added video to playlist")
 })
 
 export {
     createPlaylist,
     getUserPlaylists,
-    getPlaylistById
+    getPlaylistById,
+    addVideoToPlaylist
 }
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { createPlaylist, getPlaylistById, getUserPlaylists } from "../controllers/playlist.controller";
+import { createPlaylist, getPlaylistById, getUserPlaylists  , addVideoToPlaylist} from "../controllers/playlist.controller";
 import { protect } from "../middlewares/auth.middleware.js";
 
 
@@ -8,5 +8,6 @@ const router = Router()
 router.post("/create" , protect , createPlaylist)
 router.get("/get_user_playlists/:userId" , protect , getUserPlaylists)
 router.get("/get_playlist" , protect , getPlaylistById)
+router.patch("/add_video/:playlist/:videoId" , protect , addVideoToPlaylist)
 
 export default router
