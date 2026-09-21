0ea6e422ffce302a4d966249ff487d6434d401b6
Animal-gits <muhammadhadi8920@gmail.com>
2026-09-21 14:27:38 +0500

    removed env from server.js file

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 import {app} from './src/app.js'
 import {connectDB} from './src/config/db.js'
-import env from "./src/config/env.js"
 // import dns from "node:dns";
 
 
-dotenv.config({
-    path : './.env'
-})
 
 connectDB()
 // dns.setServers(["8.8.8.8", "1.1.1.1"]);
