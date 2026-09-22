56e0aa7b7db93d3e88c14d43052266f609c6a3a4
Animal-gits <muhammadhadi8920@gmail.com>
2026-09-22 14:37:37 +0500

    Handled the edge cases in tokens generation config

--- src/config/generateToken.js
+++ src/config/generateToken.js
@@ -5,8 +5,13 @@ import {User} from '../models/user.model.js'
 const generateAccessToken = async(userId) => {
     try {
         const user = await User.findById(userId)
-        const accessToken = user.generateAccessToken()
-    
+        if(!user){
+            throw new ApiError(404 , "User not found")
+        }
+        const accessToken = await user.generateAccessToken()
+        if(!accessToken){
+            throw new ApiError(401 , "Auth failed")
+        }
         return accessToken
     } catch (error) {
         throw new ApiError(500 , "Something went wrong while generating Access Token" || error.message)
@@ -16,8 +21,13 @@ const generateAccessToken = async(userId) => {
 const generateRefreshToken = async(userId) => {
     try {
         const user = await User.findById(userId)
+        if(!user){
+            throw new ApiError(404 , "User not found")
+        }
         const refreshToken = user.generateRefreshToken()
-
+        if(!refreshToken){
+            throw new ApiError(401 , "Auth failed")
+        }
         user.refreshToken = refreshToken
         await user.save({validateBeforeSave : false})
     } catch (error) {
